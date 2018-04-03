53ea83498f229e83093182482ecc2af4f83cfdf8
Rick Schubert <rick.schubert@arcadiagroup.co.uk>
2018-04-03 14:10:57 +0100

    Modifications

--- proxy_server/fixed_responses.py
+++ proxy_server/fixed_responses.py
@@ -5,20 +5,22 @@ import json
 import re
 
 class Injector:
-    def load(self, loader):
-        loader.add_option(
-            "iframe", str, "", "IFrame to inject"
-        )
-
     def response(self, flow: http.HTTPFlow) -> None:
-        if flow.request.url == 'http://www.example.com/':
-            flow.response.content = str("You requested example.com").encode("utf8")
+        # Topshop PDP
         if 'http://local.m.topshop.com:8080/api/products/%2Fen%' in flow.request.url:
             flow.response = http.HTTPResponse.make(
                 200,
-                json.dumps(json.load(open('proxy_server/fixed_responses/pdp.json'))),
+                json.dumps(json.load(open('proxy_server/fixed_responses/ts_pdp.json'))),
                 {"Content-Type": "application/json"}
             )
+        # Dorothy Perkins PDP - Products out of stock
+        if 'http://local.m.dorothyperkins.com:8080/api/products/%2Fen%' in flow.request.url:
+            flow.response = http.HTTPResponse.make(
+                200,
+                json.dumps(json.load(open('proxy_server/fixed_responses/dp_pdp.json'))),
+                {"Content-Type": "application/json"}
+            )
+        # Dorothy Perkins Quick View
         if re.match(r'http://local.m.dorothyperkins.com:8080/api/products/[0-9]{8}', flow.request.url):
         # if flow.request.url == 'http://local.m.dorothyperkins.com:8080/api/products/31364808':
             flow.response = http.HTTPResponse.make(
@@ -30,9 +32,10 @@ class Injector:
 addons = [Injector()]
 
 # Run this script with:
-# mitmproxy --listen-host 0.0.0.0 --listen-port 3044 -s proxy_server/index.py --anticache
-# mitmproxy --listen-host 0.0.0.0 --listen-port 3044 -s proxy_server/index.py --anticache --ignore '^(?!topshop\.com)'
-# mitmproxy --listen-host 0.0.0.0 --listen-port 3044 -s proxy_server/index.py --anticache --intercept '~a'
+# mitmweb --listen-host 0.0.0.0 --listen-port 3044 -s proxy_server/fixed_responses.py --anticache
+# mitmweb --listen-host 0.0.0.0 --listen-port 3044 -s proxy_server/fixed_responses.py --anticache --ignore '^(?!topshop\.com)'
+# mitmweb --listen-host 0.0.0.0 --listen-port 3044 -s proxy_server/fixed_responses.py --anticache --intercept '~a'
+# mitmweb --listen-host 0.0.0.0 --listen-port 3044 -s proxy_server/fixed_responses.py --anticache --set stream_large_bodies=30k
 
 # Start Chrome with proxy via
 # /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --proxy-server=http://0.0.0.0:3044
